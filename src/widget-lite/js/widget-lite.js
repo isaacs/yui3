f0a4b3e3ad5150879b992c85e79e2f92e8806fbc
isaacs <i@foohack.com>
2009-12-06 18:01:50 -0800

    Just copied over the widget stuff. No new things for now.  Starting out stubbing the bar minimums that a child class absolutely needs.

---
+++ src/widget-lite/js/widget-lite.js
@@ -0,0 +1,13 @@
+/**
+ * Provides the lite Widget class
+ *
+ * @module widget-lite
+ */
+
+var ClassNameManager = Y.ClassNameManager,
+    Node = Y.Node;
+
+function Widget () { Widget.superclass.constructor.apply(this, arguments) }
+
+
+Y.Widget = Widget;
